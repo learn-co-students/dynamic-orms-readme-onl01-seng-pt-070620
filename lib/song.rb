60dc055146b6a755865b17724d8b65453afeec7b
Zainab-Omar <zainab.alshaikhli01@gmail.com>
2020-09-11 16:09:06 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-
+require 'pry'
 class Song
 
 
@@ -20,23 +20,22 @@ class Song
     end
     column_names.compact
   end
-
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
-
+  
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
     end
   end
-
+  
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
-
+  
   def table_name_for_insert
     self.class.table_name
   end
@@ -52,7 +51,7 @@ class Song
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
-
+  
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
